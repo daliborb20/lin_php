b121341853e3ed76331b9ff82c70adb34e869bb7
Dalibor Bogicevic <daliborb20@gmail.com>
2018-11-03 09:06:39 +0100

    naslov autor iz baze

--- cms/index.php
+++ cms/index.php
@@ -7,7 +7,16 @@
 
             <!-- Blog Entries Column -->
             <div class="col-md-8">
+<?php
+$query = "select * from postovi";
+$rezultat = mysqli_query($connection, $query);
+while($red = mysqli_fetch_assoc($rezultat)){
+   $naslov = $red['post_naslov'];
+   $autor = $red['post_autor'];
+   $sadrzaj = $red['post_sadrzaj'];
+   $slika = $red['post_slika'];
 
+?>
                 <h1 class="page-header">
                     Page Heading
                     <small>Secondary Text</small>
@@ -15,21 +24,22 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#">Blog Post Title</a>
+                <a href="#"><?php echo $naslov ?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
+                by <a href="index.php"><?php echo $autor ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
                 <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="<?php echo $slika ?>" alt="">
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                <p><?php echo $sadrzaj ?></p>
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
                 <hr>
 
-            </div>
+<?php } ?>
+
+           </div>
 <?php include "./includes/sider.php"; ?>
         </div>
         <!-- /.row -->
